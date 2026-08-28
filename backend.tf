201fc031d15d831146ad37efe0a16fbf95289dc5
sridharsubaraya-collab <sridharsubaraya@gmail.com>
2026-08-28 20:39:18 +0530

    region chenged

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-secure-tf-state-23542343411"
     key            = "terraform/state"
-    region         = "eu-west-1"
+    region         = "ap-south-1"
     encrypt        = true
   }
 }
